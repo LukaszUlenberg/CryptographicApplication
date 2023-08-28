b45a06150f2bf3ec6691345931a749073262f26a
lukasz <lukasz@koni.pl>
2023-08-28 13:33:02 +0200

    Dodanie przycisków wraz z metodami je obsługującymi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonCreateAsmKeys = new Button();
+            label1 = new Label();
+            buttonEncryptFile = new Button();
+            buttonDecryptFile = new Button();
+            buttonExportPublicKey = new Button();
+            _encryptOpenFileDialog = new OpenFileDialog();
+            _decryptOpenFileDialog = new OpenFileDialog();
+            buttonGetPrivateKey = new Button();
+            buttonImportPublicKe = new Button();
+            SuspendLayout();
+            // 
+            // buttonCreateAsmKeys
+            // 
+            buttonCreateAsmKeys.Location = new Point(65, 60);
+            buttonCreateAsmKeys.Name = "buttonCreateAsmKeys";
+            buttonCreateAsmKeys.Size = new Size(219, 23);
+            buttonCreateAsmKeys.TabIndex = 0;
+            buttonCreateAsmKeys.Text = "Tworzenie klucza asymetrycznego";
+            buttonCreateAsmKeys.UseVisualStyleBackColor = true;
+            buttonCreateAsmKeys.Click += buttonCreateAsmKeys_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(65, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // buttonEncryptFile
+            // 
+            buttonEncryptFile.Location = new Point(65, 104);
+            buttonEncryptFile.Name = "buttonEncryptFile";
+            buttonEncryptFile.Size = new Size(219, 23);
+            buttonEncryptFile.TabIndex = 2;
+            buttonEncryptFile.Text = "Szyfrowanie pliku";
+            buttonEncryptFile.UseVisualStyleBackColor = true;
+            buttonEncryptFile.Click += buttonEncryptFile_Click;
+            // 
+            // buttonDecryptFile
+            // 
+            buttonDecryptFile.Location = new Point(65, 150);
+            buttonDecryptFile.Name = "buttonDecryptFile";
+            buttonDecryptFile.Size = new Size(219, 23);
+            buttonDecryptFile.TabIndex = 3;
+            buttonDecryptFile.Text = "Odszyfrowywanie pliku";
+            buttonDecryptFile.UseVisualStyleBackColor = true;
+            buttonDecryptFile.Click += buttonDecryptFile_Click;
+            // 
+            // buttonExportPublicKey
+            // 
+            buttonExportPublicKey.Location = new Point(65, 200);
+            buttonExportPublicKey.Name = "buttonExportPublicKey";
+            buttonExportPublicKey.Size = new Size(219, 23);
+            buttonExportPublicKey.TabIndex = 4;
+            buttonExportPublicKey.Text = "Eksportowanie klucza publicznego";
+            buttonExportPublicKey.UseVisualStyleBackColor = true;
+            buttonExportPublicKey.Click += buttonExportPublicKey_Click;
+            // 
+            // _encryptOpenFileDialog
+            // 
+            _encryptOpenFileDialog.FileName = "_encryptOpenFileDialog";
+            // 
+            // _decryptOpenFileDialog
+            // 
+            _decryptOpenFileDialog.FileName = "_decryptOpenFileDialog";
+            // 
+            // buttonGetPrivateKey
+            // 
+            buttonGetPrivateKey.Location = new Point(65, 275);
+            buttonGetPrivateKey.Name = "buttonGetPrivateKey";
+            buttonGetPrivateKey.Size = new Size(219, 23);
+            buttonGetPrivateKey.TabIndex = 5;
+            buttonGetPrivateKey.Text = "Uzyskiwanie klucza prywatnego";
+            buttonGetPrivateKey.UseVisualStyleBackColor = true;
+            buttonGetPrivateKey.Click += buttonGetPrivateKey_Click;
+            // 
+            // buttonImportPublicKe
+            // 
+            buttonImportPublicKe.Location = new Point(65, 239);
+            buttonImportPublicKe.Name = "buttonImportPublicKe";
+            buttonImportPublicKe.Size = new Size(219, 23);
+            buttonImportPublicKe.TabIndex = 6;
+            buttonImportPublicKe.Text = "Importowanie klucza publicznego";
+            buttonImportPublicKe.UseVisualStyleBackColor = true;
+            buttonImportPublicKe.Click += buttonImportPublicKe_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonImportPublicKe);
+            Controls.Add(buttonGetPrivateKey);
+            Controls.Add(buttonExportPublicKey);
+            Controls.Add(buttonDecryptFile);
+            Controls.Add(buttonEncryptFile);
+            Controls.Add(label1);
+            Controls.Add(buttonCreateAsmKeys);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonCreateAsmKeys;
+        private Label label1;
+        private Button buttonEncryptFile;
+        private Button buttonDecryptFile;
+        private Button buttonExportPublicKey;
+        private OpenFileDialog _encryptOpenFileDialog;
+        private OpenFileDialog _decryptOpenFileDialog;
+        private Button buttonGetPrivateKey;
+        private Button buttonImportPublicKe;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,291 @@
+using System.Security.Cryptography;
+using System.Text.Unicode;
+
 namespace CryptographicApplication
 {
     public partial class Form1 : Form
     {
+        // Zadeklaruj obiekty CspParameters i RsaCryptoServiceProvider
+        // z globalnym zakresem klasy formularza.
+        readonly CspParameters _cspp = new CspParameters();
+        RSACryptoServiceProvider _rsa;
+
+        // Zmienne œcie¿ek dla folderu Ÿród³owego, szyfruj¹cego i
+        // folderów deszyfrowania. Musz¹ koñczyæ siê odwrotnym ukoœnikiem.
+        const string EncrFolder = @"c:\Encrypt\";
+        const string DecrFolder = @"c:\Decrypt\";
+        const string SrcFolder = @"c:\docs\";
+
+        // Plik klucza publicznego
+        const string PubKeyFile = @"c:\encrypt\rsaPublicKey.txt";
+
+        // Nazwa kontenera kluczy dla
+        // pary wartoœci klucza prywatnego/publicznego.
+        const string KeyName = "Key01";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonCreateAsmKeys_Click(object sender, EventArgs e)
+        {
+            // Przechowuje parê kluczy w kontenerze kluczy.
+            _cspp.KeyContainerName = KeyName;
+            _rsa = new RSACryptoServiceProvider(_cspp)
+            {
+                PersistKeyInCsp = true
+            };
+
+            label1.Text = _rsa.PublicOnly
+                ? $"Key: {_cspp.KeyContainerName} - Tylko publiczny"
+                : $"Key: {_cspp.KeyContainerName} - Pe³na para kluczy";
+        }
+
+        private void buttonEncryptFile_Click(object sender, EventArgs e)
+        {
+            if (_rsa is null)
+            {
+                MessageBox.Show("Klucz nie zosta³ ustawiony.");
+            }
+            else
+            {
+                // Wyœwietl okno dialogowe, aby wybraæ plik do zaszyfrowania.
+                _encryptOpenFileDialog.InitialDirectory = SrcFolder;
+                if (_encryptOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = _encryptOpenFileDialog.FileName;
+                    if (fName != null)
+                    {
+                        // Przeka¿ nazwê pliku bez œcie¿ki.
+                        EncryptFile(new FileInfo(fName));
+                    }
+                }
+            }
+        }
+
+        private void EncryptFile(FileInfo file)
+        {
+            // Tworzenie instancji Aes dla
+            // symetrycznego szyfrowania danych.
+            Aes aes = Aes.Create();
+            ICryptoTransform transform = aes.CreateEncryptor();
+
+            // U¿yj RSACryptoServiceProvider do
+            // zaszyfrowania klucza AES.
+            // rsa zosta³ wczeœniej utworzony:
+            // rsa = new RSACryptoServiceProvider(cspp);
+            byte[] keyEncrypted = _rsa.Encrypt(aes.Key, false);
+
+            // Utwórz tablice bajtów zawieraj¹ce
+            // wartoœci d³ugoœci klucza i IV.
+            int lKey = keyEncrypted.Length;
+            byte[] LenK = BitConverter.GetBytes(lKey);
+            int lIV = aes.IV.Length;
+            byte[] LenIV = BitConverter.GetBytes(lIV);
+
+            // Zapis nastêpuj¹cych danych do FileStream
+            // dla zaszyfrowanego pliku (outFs):
+            // - d³ugoœæ klucza
+            // - d³ugoœæ IV
+            // - zaszyfrowany klucz
+            // - IV
+            // - zaszyfrowana zawartoœæ szyfru
+
+            // Zmieñ rozszerzenie pliku na ".enc"
+            string outFile = Path.Combine(EncrFolder, Path.ChangeExtension(file.Name, ".enc"));
+
+            using (var outFs = new FileStream(outFile, FileMode.Create))
+            {
+                outFs.Write(LenK, 0, 4);
+                outFs.Write(LenIV, 0, 4);
+                outFs.Write(keyEncrypted, 0, lKey);
+                outFs.Write(aes.IV, 0, lIV);
+
+                // Teraz zapisz tekst zaszyfrowany przy u¿yciu
+                // CryptoStream do szyfrowania.
+                using (var outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                {
+                    // Szyfruj¹c fragment po fragmencie
+                    // jednorazowo, mo¿na zaoszczêdziæ pamiêæ
+                    // i pomieœciæ du¿e pliki.
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes mo¿e mieæ dowolny rozmiar.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+                    int bytesRead = 0;
+
+                    using (var inFs = new FileStream(file.FullName, FileMode.Open))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamEncrypted.Write(data, 0, count);
+                            bytesRead += blockSizeBytes;
+                        } while (count > 0);
+                    }
+                    outStreamEncrypted.FlushFinalBlock();
+                }
+            }
+        }
+
+        private void buttonDecryptFile_Click(object sender, EventArgs e)
+        {
+            if (_rsa is null)
+            {
+                MessageBox.Show("Klucz nie zosta³ ustawiony.");
+            }
+            else
+            {
+                // Wyœwietl okno dialogowe, aby wybraæ zaszyfrowany plik.
+                _decryptOpenFileDialog.InitialDirectory = EncrFolder;
+                if (_decryptOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fName = _decryptOpenFileDialog.FileName;
+                    if (fName != null)
+                    {
+                        DecryptFile(new FileInfo(fName));
+                    }
+                }
+            }
+        }
+
+        private void DecryptFile(FileInfo file)
+        {
+            // Tworzenie instancji Aes dla
+            // symetrycznego deszyfrowania danych.
+            Aes aes = Aes.Create();
+
+            // Utwórz tablice bajtów, aby uzyskaæ d³ugoœæ
+            // zaszyfrowanego klucza i IV.
+            // Wartoœci te zosta³y zapisane jako 4 bajty ka¿da
+            // na pocz¹tku zaszyfrowanego pakietu.
+            byte[] LenK = new byte[4];
+            byte[] LenIV = new byte[4];
+
+            // Konstruujemy nazwê odszyfrowanego pliku.
+            string outFile = Path.ChangeExtension(file.FullName.Replace("Encrypt", "Decrypt"), ".txt");
+
+            // U¿yj obiektów FileStream do odczytania zaszyfrowanego pliku
+            // pliku (inFs) i zapisania odszyfrowanego pliku (outFs).
+            using (var inFs = new FileStream(file.FullName, FileMode.Open))
+            {
+                inFs.Seek(0, SeekOrigin.Begin);
+                inFs.Read(LenK, 0, 3);
+                inFs.Seek(4, SeekOrigin.Begin);
+                inFs.Read(LenIV, 0, 3);
+
+                // Konwersja d³ugoœci na wartoœci ca³kowite.
+                int lenK = BitConverter.ToInt32(LenK, 0);
+                int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                // Okreœlenie pozycji pocz¹tkowej
+                // tekstu zaszyfrowanego (startC)
+                // i jego d³ugoœæ (lenC).
+                int startC = lenK + lenIV + 8;
+                int lenC = (int)inFs.Length - startC;
+
+                // Tworzenie tablic bajtów dla
+                // zaszyfrowanego klucza Aes,
+                // IV i tekstu zaszyfrowanego.
+                byte[] KeyEncrypted = new byte[lenK];
+                byte[] IV = new byte[lenIV];
+
+                // Wyodrêbnienie klucza i IV
+                // zaczynaj¹c od indeksu 8
+                // po wartoœciach d³ugoœci.
+                inFs.Seek(8, SeekOrigin.Begin);
+                inFs.Read(KeyEncrypted, 0, lenK);
+                inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                inFs.Read(IV, 0, lenIV);
+
+                Directory.CreateDirectory(DecrFolder);
+                // U¿yj RSACryptoServiceProvider
+                // do odszyfrowania klucza AES.
+                byte[] KeyDecrypted = _rsa.Decrypt(KeyEncrypted, false);
+
+                // Odszyfrowanie klucza.
+                ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV);
+
+                // Odszyfruj tekst zaszyfrowany z
+                // z FileSteam zaszyfrowanego pliku
+                // zaszyfrowanego pliku (inFs) do FileStream
+                // dla odszyfrowanego pliku (outFs).
+                using (var outFs = new FileStream(outFile, FileMode.Create))
+                {
+                    int count = 0;
+                    int offset = 0;
+
+                    // blockSizeBytes mo¿e mieæ dowolny rozmiar.
+                    int blockSizeBytes = aes.BlockSize / 8;
+                    byte[] data = new byte[blockSizeBytes];
+
+                    // Odszyfrowuj¹c jeden fragment na raz,
+                    // mo¿na zaoszczêdziæ pamiêæ i
+                    // pomieœciæ du¿e pliki.
+
+                    // Rozpocznij od pocz¹tku
+                    // tekstu zaszyfrowanego.
+                    inFs.Seek(startC, SeekOrigin.Begin);
+
+                    using (var outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                    {
+                        do
+                        {
+                            count = inFs.Read(data, 0, blockSizeBytes);
+                            offset += count;
+                            outStreamDecrypted.Write(data, 0, count);
+                        } while (count > 0);
+
+                        outStreamDecrypted.FlushFinalBlock();
+                    }
+                }
+            }
+        }
+
+        private void buttonExportPublicKey_Click(object sender, EventArgs e)
+        {
+            // Zapisz klucz publiczny utworzony przez RSA
+            // do pliku. Uwaga, zapisanie klucza
+            // klucza do pliku stanowi zagro¿enie dla bezpieczeñstwa.
+            Directory.CreateDirectory(EncrFolder);
+            using (var sw = new StreamWriter(PubKeyFile, false))
+            {
+                sw.Write(_rsa.ToXmlString(false));
+            }
+        }
+
+        private void buttonImportPublicKe_Click(object sender, EventArgs e)
+        {
+            using (var sr = new StreamReader(PubKeyFile))
+            {
+                _cspp.KeyContainerName = KeyName;
+                _rsa = new RSACryptoServiceProvider(_cspp);
+
+                string keytxt = sr.ReadToEnd();
+                _rsa.FromXmlString(keytxt);
+                _rsa.PersistKeyInCsp = true;
+
+                label1.Text = _rsa.PublicOnly
+                    ? $"Key: {_cspp.KeyContainerName} - Public Only"
+                    : $"Key: {_cspp.KeyContainerName} - Full Key Pair";
+            }
+        }
+
+        private void buttonGetPrivateKey_Click(object sender, EventArgs e)
+        {
+            _cspp.KeyContainerName = KeyName;
+            _rsa = new RSACryptoServiceProvider(_cspp)
+            {
+                PersistKeyInCsp = true
+            };
+
+            label1.Text = _rsa.PublicOnly
+                ? $"Key: {_cspp.KeyContainerName} - Public Only"
+                : $"Key: {_cspp.KeyContainerName} - Full Key Pair";
         }
     }
 }
